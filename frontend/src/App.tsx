f600f9ed33effd850c79194fc570a8654e629197
Subhadeep Saha <rajsaha.sep@gmail.com>
2026-01-07 15:27:35 +0530

    feat: access microphone using MediaStream API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [micStatus, setMicStatus] = useState("Requesting microphone access...");
+
+  useEffect(() => {
+    const initMicrophone = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        });
+
+        if (stream) {
+          setMicStatus("Microphone access granted");
+        }
+      } catch (error) {
+        console.error(error);
+        setMicStatus("Microphone access denied");
+      }
+    };
+
+    initMicrophone();
+  }, []);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-900 text-slate-100">
       <div className="text-center">
-        <h1 className="text-3xl font-bold mb-2">
+        <h1 className="text-3xl font-bold mb-3">
           Audio Transcription Platform
         </h1>
-        <p className="text-slate-400">
-          Tailwind CSS installed using official docs
-        </p>
+        <p className="text-lg">{micStatus}</p>
       </div>
     </div>
   );
